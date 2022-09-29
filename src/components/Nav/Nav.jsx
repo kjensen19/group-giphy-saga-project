6818b7adbd26e46873e2608acabb839daeaf5904
Maggie Whitlock <maggie.whitlock@gmail.com>
2022-09-29 14:44:56 -0500

    favorite GET

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,12 +1,35 @@
-import Search from "../Search/Search";
-import Favs from "../Favs/Favs";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
 
-function Nav() {
+function Nav(){
+    const [searchTerm, setSearchTerm] = useState('');
+    const dispatch = useDispatch();
+
+    const searchForGifs = () => {
+        console.log(searchTerm);
+        dispatch({
+            type: 'SEARCH_GIFS_S',
+            payload: searchTerm
+        });
+    };
+
+    const showFaves = () => {
+        console.log('in showFaves')
+        dispatch({
+            type: 'GET_FAVES_S'
+        })
+    };
+
     return (
-        <>
-            <Search />
-            <Favs />
-        </>
+        <div className="searchSection">
+            <input
+                type="text"
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}></input>
+            <button onClick={searchForGifs}>Submit</button>
+            <button onClick={showFaves}>Show Favorites</button>
+        </div>
     )
 }
-export default Nav
+
+export default Nav;
